037ad91f40afc071ba9140f2f96201c69ef8fa77
PalomaAbdurramaniGitHub <paloma.abdurramani@fshnstudent.info>
2024-10-16 23:03:58 +0200

    handlers

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Post' },
     authorId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    commentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
+    commentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null },
     content: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
