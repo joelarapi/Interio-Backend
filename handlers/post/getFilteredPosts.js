45cb40ab201bccbf61d60e3afa6ad32be5fb85e8
PalomaAbdurramaniGitHub <paloma.abdurramani@fshnstudent.info>
2024-10-21 17:36:40 +0200

    filter

--- handlers/post/getFilteredPosts.js
+++ handlers/post/getFilteredPosts.js
@@ -34,10 +34,33 @@ export const handler = async (event) => {
             filters.createdAt = { $gte: dateLimit };
         }
 
-        const filteredPosts = await Post.find(filters)
-            .populate('category')
-            .populate('offers')
-            .exec();
+        const filteredPosts = await Post.find(filters);
+
+        try {
+            await filteredPosts.populate('offers');
+        } catch (populateError) {
+            console.error("Error populating offers:", populateError);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Error during category population', error: populateError.message }),
+                headers: {
+                    "Access-Control-Allow-Origin": '*'
+                }
+            };
+        }
+
+        try {
+            await filteredPosts.populate('category');
+        } catch (populateError) {
+            console.error("Error populating category:", populateError);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Error during bookmarks population', error: populateError.message }),
+                headers: {
+                    "Access-Control-Allow-Origin": '*'
+                }
+            };
+        }
 
         if (!filteredPosts || filteredPosts.length === 0) {
             return {
