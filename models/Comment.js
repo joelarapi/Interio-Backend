37f337bbf4b86dbf1e958dcd338dd5f5f68dffa0
PalomaAbdurramaniGitHub <paloma.abdurramani@fshnstudent.info>
2024-10-22 11:40:14 +0200

    user type

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Post' },
-    authorId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    authorId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    authorType: { type: String, enum: ['User', 'Business'], required: true },
     commentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null },
     content: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
--- models/Notifications.js
+++ models/Notifications.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const notificationsSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    userType: { type: String, enum: ['User', 'Business'], required: true },
     type: { type: String, enum: ['offer', 'post', 'message', 'comment', 'info'], required: true, default: 'info' },
     content: { type: String, required: true },
     isRead: { type: Boolean, default: false },
