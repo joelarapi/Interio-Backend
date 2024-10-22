dc8da79f2fe21440cddd3e4f1eb89eb7ea819964
PalomaAbdurramaniGitHub <paloma.abdurramani@fshnstudent.info>
2024-10-22 17:11:14 +0200

    end date

--- models/SubscriptionManager.js
+++ models/SubscriptionManager.js
@@ -4,7 +4,15 @@ const subscriptionManagerSchema = new mongoose.Schema({
     businessId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Business', unique: true },
     subscriptionId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'SubscriptionPlan' },
     startDate: { type: Date, default: Date.now },
-    endDate: { type: Date },
+    endDate: { 
+        type: Date, 
+        default: function() {
+            const startDate = this.startDate || new Date();
+            const endDate = new Date(startDate);
+            endDate.setMonth(endDate.getMonth() + 1);  // 1 month from the start date
+            return endDate;
+        }
+    },
     offerNumberLimit: { type: Number },
     offerNumberCount: { type: Number, default: 0 },
     setAutoRenew: { type: Boolean, default: false },
